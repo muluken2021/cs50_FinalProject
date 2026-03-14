3270989e949d1c6d9cc545d1367fff9c5202fe2f
mulukenkassaw2025 <mulukenkassaw2021@gmail.com>
2026-03-14 17:39:43 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,39 +4,7 @@ import Register from "./components/Register";
 import AddTransaction from "./components/AddTransaction";
 import TransactionList from "./components/TransactionList";
 import { getTransactions } from "./services/api";
-import AffinityCarousel from "./components/AffinityCarousel";
-import VideoTabs from "./components/VideoTabs";
-import FocusCarousel from "./components/FocusCarousel";
-import GroceryHero from "./components/GroceryHero";
-import LuxuryCarHero from "./components/LuxuryCarHero";
-import InspirationSection from "./components/InspirationSection";
-import AppleEarbudsHero from "./components/AppleEarbudsHero";
-import KittlChat from "./components/KittlChat";
-import Navbar from "./components/Navbar";
-import KittlHero from "./components/KittlHero";
-import MockupFeature from "./components/MockupFeature";
-import ContactSection from "./components/ContactSection";
-import WibifyNavbar from "./components/WibifyNavbar";
-import Testimonials from "./components/Testimonials";
-import PortfolioGrid from "./components/PortfolioGrid";
-import HighEndPortfolio from "./components/HighEndPortfolio";
-import CinematicProductScroll from "./components/CinematicProductScroll";
-import OverlapScroll from "./components/OverlapScroll";
-import DownloadQR from "./components/DownloadQR";
-import CinematicHero from "./components/CinematicHero";
-import StatsSection from "./components/StatsSection";
-import IntegrationSection from "./components/IntegrationSection";
-import PricingSection from "./components/PricingSection";
-import FAQSection from "./components/FAQSection";
-import ForgotPassword from "./components/ForgotPassword";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
-import CheckoutPage from "./components/CheckoutPage";
-import { NotFoundPage } from "./components/NotFoundPage";
-import BlogSection from "./components/BlogSection";
-import BlogPostDetail from "./components/BlogPostDetail";
-import ProductDetail from "./components/ProductDetail";
-import Product from "./components/Product";
+
 
 function App() {
   const [userId, setUserId] = useState(localStorage.getItem("userId") || null);
@@ -55,50 +23,12 @@ function App() {
 
   if (!userId) {
     return showRegister ? (
-      // <div className="flex flex-col items-center justify-center h-screen">
-      //   <Register onRegisterSuccess={() => setShowRegister(false)} />
-      //   <button onClick={() => setShowRegister(false)} className="mt-2 text-blue-500">Go to Login</button>
-        
-      // </div>
-      <div>
-        {/* <Navbar /> */}
-        <GroceryHero />
-        <LuxuryCarHero />
-        <InspirationSection />
-        <AffinityCarousel />
-        <VideoTabs />
-        <FocusCarousel />
-        <AppleEarbudsHero />
-        <KittlChat />
-        <KittlHero />
-        <MockupFeature />
-        <ContactSection />
-        <WibifyNavbar />
-        <PortfolioGrid />
-        <Testimonials />
-        <DownloadQR/>
-        <CinematicHero />
-        <StatsSection />
-        <IntegrationSection />
-        <PricingSection />
-        <FAQSection />
-        <ForgotPassword />
-        <SignIn />
-        <SignUp />
-        <CheckoutPage />
-        <BlogSection />
-        <BlogPostDetail />
-        <ProductDetail />
-        <Product />
-
-        <NotFoundPage />
-        <HighEndPortfolio/>
-         <OverlapScroll />
-        <CinematicProductScroll />
-       
-
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Register onRegisterSuccess={() => setShowRegister(false)} />
+        <button onClick={() => setShowRegister(false)} className="mt-2 text-blue-500">Go to Login</button>
         
       </div>
+
 
     ) : (
       <div className="flex flex-col items-center justify-center h-screen">
